7cbc3a0afb6806ccebe98c6d496af5accdf8f091
Ricardo Alves <ricbatera@bol.com.br>
2021-08-21 21:57:46 -0300

    modal inicial

--- js/script.js
+++ js/script.js
@@ -1,20 +1,53 @@
-console.log("Iniciando a aplicação JS")
+// VARÍAVEIS DOS ELEMENTOS DA TELA
+const modal = document.getElementById('modal');
+const status = document.getElementById('status');
+const tempo = document.getElementById('tempo');
+const concluido = document.getElementById('concluido');
+const qtdePostos = document.getElementById('qtde-postos');
+
+
+// CONEXÃO COM BACKEND
+
 const url = 'https://backend-postos.herokuapp.com/upload';
 
 var xhttp = new XMLHttpRequest();
 
 xhttp.onreadystatechange = ()=>{
     if(xhttp.readyState === XMLHttpRequest.LOADING){
-        console.log('solicitação de processamento')
+        status.innerHTML = "Carregando a lista de postos..";
     }else if(xhttp.readyState === XMLHttpRequest.OPENED ){
-        console.log('conexão do servidor estabelecida');
+        status.innerHTML = "Conexão do servidor estabelecida...";
     }else if(xhttp.readyState === XMLHttpRequest.HEADERS_RECEIVED){
-        console.log('cabeçalhos e status estão disponíveis.');
+        status.innerHTML = "Recebendo dados...";
     }else if(xhttp.readyState === XMLHttpRequest.DONE && xhttp.status === 200){
-        console.log('A operação está concluída.');
         const resposta = JSON.parse(xhttp.responseText);
+        status.innerHTML = "Pronto!";
+        concluido.classList = "";
+        qtdePostos.innerHTML = resposta.length;
+        tempoFecharBtn();
     }
 }
 
 xhttp.open('GET', url, true);
 xhttp.send();
+
+
+// FIM DA CONEXÃO COM O BACK END
+
+
+function btnFechar(){
+    modal.classList += " esconde";
+}
+
+function tempoFecharBtn(){
+    let contador = 5; 
+    const contagem = setInterval(() => {
+        if(contador == 0){
+            clearInterval(contagem);
+            btnFechar()
+        }else{
+            contador --;
+            tempo.innerHTML = contador;
+        }
+    }, 1000);
+}
